4bbbb1349dc4a9aa3ae89917d247be4393a55c39
Rayna <rayna.mecham@gmail.com>
2021-02-03 04:56:00 +0000

    finished exercise

--- ratings.py
+++ ratings.py
@@ -1,4 +1,20 @@
-"""Restaurant rating lister."""
+def get_ratings(file):
+    """Restaurant rating lister."""
+    rating = open(file)
+    restaurant_rating = {}
+
+    for line in rating:
+	    line = line.rstrip()
+	    restaurants, score = line.split(":")
+	    restaurant_rating[restaurants] = int(score)
+
+    
+    for key, value in sorted(restaurant_rating.items()):
+        print(f"{key} is rated at {value}")
+
+    return restaurant_rating
+
+get_ratings("scores.txt")
 
 
-# put your code here
+# # put your code here
